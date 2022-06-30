d8f0a65f7c9d361a45f9aa57e720a830387ef072
Eric <Ericthabiso55@gmail.com>
2022-06-30 09:28:51 -0700

    This is a script that prints the size of various data types on the computer it is compiled and run on

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - prints to the screen using puts function
+ *
+ *Return: If successful the exit status is zero
+ *
+ */
+
+int main(void)
+
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
+
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
+
+	return (0);
+
+}
