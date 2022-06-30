82c0c3eb03676121673da9821ee63bab78dcb58f
Eric <Ericthabiso55@gmail.com>
2022-06-30 09:36:00 -0700

    This is a script that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - prints to the screen using puts function
+ *
+ *Return: If successful the exit status is zero
+ *
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+
+}
