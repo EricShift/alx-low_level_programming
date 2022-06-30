2de76179f5bba70b2213f109728d86964ec665e9
Eric <Ericthabiso55@gmail.com>
2022-06-30 08:51:41 -0700

    This is a script that prints exactly "Programming is like building a multilingual puzzle, followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
+
+
+/**
+ *
+ *  * main - prints to the screen using puts function
+ *
+ *   * Return: If successful the exit status is zero
+ *
+ *    */
+
 int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle\n");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+
+
+			return (0);
 
 }
