4de3592251e761a125432e8be64d3086fc0ecabc
Eric <Ericthabiso55@gmail.com>
2022-06-30 09:10:06 -0700

    This is a script that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - prints to the screen using puts function
+ *
+ *Return: If successful the exit status is zero
+ *
+ */
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+
+	return (0);
+
+}
