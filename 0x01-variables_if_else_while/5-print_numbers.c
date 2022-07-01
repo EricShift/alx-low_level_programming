03b86b7ab95a659b419a432644c79a0f6de355e6
Eric <Ericthabiso55@gmail.com>
2022-07-01 14:28:16 -0700

    printing the all single digit numbers of base 10 starting from 0, followed by a new line

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ *main - prints to the screen using puts function
+ *
+ *Return: If successful the exit status is zero
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+
+	{
+
+		printf("%d"i);
+
+	}
+
+		putchar('\n');
+
+return (0);
+
+}
