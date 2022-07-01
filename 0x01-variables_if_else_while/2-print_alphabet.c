ddc99ee613a69f142c3da100375d7c7fb0ecf3cc
Eric <Ericthabiso55@gmail.com>
2022-07-01 13:19:26 -0700

    printing the alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ *main - prints to the screen using puts function
+ *
+ *Return: If successful the exit status is zero
+ *
+ */
+
+int main(void)
+
+{
+
+	int n;
+	for(int x = 'A'; x <= 'Z'; x++)
+
+	{
+
+		putchar(tolower(x));
+
+	}
+		putchar('\n');
+return (0);
+
+}
