6c86aa2da85a5af5c88f7d6d589a5f9df925f4ed
Eric <Ericthabiso55@gmail.com>
2022-07-01 15:28:29 -0700

    printing all the numbers of base 16 in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ *main - prints to the screen using puts function
+ *
+ *Return: If successful the exit status is zero
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+
+	{
+
+		putchar(i);
+
+	}
+
+	for (i = 97; i <= 102; i++)
+
+	{
+		putchar(i);
+	}
+
+		putchar('\n');
+
+return (0);
+
+}
