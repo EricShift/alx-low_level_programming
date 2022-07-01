d4dae3ba391cdb7cf2c4e223b9ebac71d21c20e1
Eric <Ericthabiso55@gmail.com>
2022-07-01 15:09:38 -0700

    printing the lowercase alphabet in reverse, followed by a new line

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ *main - prints to the screen using puts function
+ *
+ *Return: If successful the exit status is zero
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+
+	{
+
+		putchar(i);
+
+	}
+
+		putchar('\n');
+
+return (0);
+
+}
