dee4d2617d8cd90af18816f3495702a4a1df2a40
Eric <Ericthabiso55@gmail.com>
2022-07-07 06:01:41 -0700

    prints the numbers, from 0 to 9 except 2 & 4, followed by a new line

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_most_numbers - program to print 0-9 except 2 and 4
- * Return: Null void
- */
+* print_most_numbers - program to print 0-9 except 2 and 4
+* Return: Null Void
+*/
 
 void print_most_numbers(void)
 {
@@ -11,7 +11,7 @@ void print_most_numbers(void)
 
 	for (i = 48; i <= 57; i++)
 	{
-		if (i == 50 ) || (i == 52)
+		if (i == 50 || i == 52)
 		{
 			continue;
 		}
