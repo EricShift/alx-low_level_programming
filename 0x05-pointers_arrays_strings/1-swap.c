a93f66409d7dbacb8b981d53f75aadf29742a939
Eric <Ericthabiso55@gmail.com>
2022-07-11 11:46:50 -0700

    swaps the values of two integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,7 +3,6 @@
  * swap_int - This changes the positions of the values
  * @a: This is a pointer to the first value
  * @b: This is a pointer to the second value
- * @c: This holds the first value for later use
  *
  * Return: Null void
  */
