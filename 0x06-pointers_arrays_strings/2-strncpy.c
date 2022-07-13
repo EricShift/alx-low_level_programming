3eff12b910e5979b6e2014178746c7d87ab9f079
Eric <Ericthabiso55@gmail.com>
2022-07-13 04:52:33 -0700

    copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - Concatenates one string to another
+ * @dest: This is the string to be copied to
+ * @src: This is the string to be copied
+ * @n: number of bytes to be copied
+ *
+ * Return: The destination string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+	char *strncpy(char *dest, const char *src, size_t n);
+
+return (dest);
+}
