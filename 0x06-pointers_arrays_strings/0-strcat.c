4d8c6ee758b56e25870e6e02ab165811d3ad2415
Eric <Ericthabiso55@gmail.com>
2022-07-13 04:27:37 -0700

    appends the src string to the dest string, overwriting the terminating null byte (\0) at the end of dest, and then adds a terminating null byte

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - Concatenates one string to another
+ * @dest: This is the string to be concatenated to
+ * @src: This is the string to be concatenated
+ * Return: The destination string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *strcat(char *restrict dest, const char *restrict src);
+
+	return (dest);
+}
