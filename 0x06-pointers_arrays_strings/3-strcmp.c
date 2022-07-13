ef68c557804bb0f0ee4a616680860b2945636df6
Eric <Ericthabiso55@gmail.com>
2022-07-13 09:20:53 -0700

    compares two strings.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,19 +1,27 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strcmp - This compares two strings
  * @s1: This is the first string
  * @s2: This is the second string
  *
- * Return: compared results 
+ * Return: An int, 0 if equal, +ve is 1 > 2
+ * -ve if 1 < 2
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int comp;
+	int count = 0, count2 = 0;
 
-	comp = strcmp(s1, s2);
+	while (count == 0)
+	{
+		if ((*(s1 + count2) == '\0') && (*(s2 + count2) == '\0'))
 
-return (comp);
+			break;
+
+		count = *(s1 + count2) - *(s2 + count2);
+
+		count2++;
+
+	return (count);
 }
