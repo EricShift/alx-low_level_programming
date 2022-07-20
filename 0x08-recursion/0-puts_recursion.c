6595f315c57ef013210c752d6ed5aa483c17c4af
Eric <Ericthabiso55@gmail.com>
2022-07-20 09:24:35 -0700

    prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts_recursion - rints a string, followed by a new line
+ *
+ * @s: string
+ *
+ * Return: returns Null
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(*s);
+	_putchar('\n');
+
+}
