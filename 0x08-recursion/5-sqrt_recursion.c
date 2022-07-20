134acda22655949ace54833919495bd07a913997
Eric <Ericthabiso55@gmail.com>
2022-07-20 10:18:31 -0700

    returns the natural square root of a number and If n does not have a natural square root, the function should return -1

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <math.h>
+
+/** 
+ * qrt_recursion - Returns to the main program
+ * the number from _sqrt
+ * @n: this is the number
+ *
+ * Return: An int
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (sqrt(n, 1));
+}
